8867c2a30ea31802b438a46675f35289a9e7638f
kpmy <petryxa.clever@gmail.com>
2015-01-06 22:15:01 +0300

    кодовые файлы перенес в отдельный каталог code

--- xev/cmds.go
+++ xev/cmds.go
@@ -7,10 +7,12 @@ import (
 	"path/filepath"
 )
 
+const CODE = "code"
+
 func Load(path, name string) (ret *module.Module) {
 	fmt.Println(path + ` ` + name)
 	var data []byte
-	data, _ = ioutil.ReadFile(filepath.Join(path, name))
+	data, _ = ioutil.ReadFile(filepath.Join(path, CODE, name))
 	fmt.Println(len(data))
 	if data != nil {
 		result := LoadOXF(data)
